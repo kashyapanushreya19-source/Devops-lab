2bc4444bd38cada2d174bcae242aa9c75bf5bfd1
Anushreya <kashyapanushreya19@gmail.com>
2026-03-25 14:32:13 +0530

    hello

---
+++ app.py
@@ -0,0 +1,8 @@
+from flask import Flask , render_template  #import the file 
+app= Flask(__name__) #to store the done done function 
+@app.route("/")#specify where my app needs to go to 
+def home():#what the application is supposed to do
+    return render_template("html_structure.html")
+
+if __name__=="__main__":
+    app.run(debug=True) #if this variable actually contaicn main function
